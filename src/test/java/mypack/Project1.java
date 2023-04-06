1c7791f3da00890c17aaa4c2c06c3abfd7c3f85b
LENOVO <LENOVO@DESKTOP-BPQ27MO>
2023-04-06 16:05:42 +0530

    I am adding new project

---
+++ src/test/java/mypack/Project1.java
@@ -0,0 +1,32 @@
+package mypack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Project1 {
+public RemoteWebDriver driver;
+@BeforeClass
+public void beforeMethod() {
+WebDriverManager.chromedriver().setup();
+driver=new ChromeDriver();
+}
+@Test
+public void method() throws InterruptedException{
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	Thread.sleep(3000);
+	driver.findElement(By.name("username")).sendKeys("Admin");
+	driver.findElement(By.name("password")).sendKeys("admin123");
+	driver.findElement(By.xpath("//button[@type='submit']")).click();
+	Thread.sleep(5000);
+	driver.quit();
+	
+}
+
+	}
+
+
